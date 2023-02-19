637ac0992fd484fe097241385d6a7b35c6aaa947
Cody Constant <astraeusdev@gmail.com>
2023-02-19 04:25:39 +0000

    change(spotify-util): add constants for api url and local storage keys

--- src/utilities/spotify.ts
+++ src/utilities/spotify.ts
@@ -1,1 +1,13 @@
+const BASE_API_URL = "https://api.spotify.com/v1";
+
+interface IStorageKeys {
+  accessToken: string;
+  exp_timestamp: string;
+  token_type: string;
+}
+const STORAGE_KEYS: IStorageKeys = {
+  accessToken: "SPOTIFY_ACCESS_TOKEN",
+  exp_timestamp: "SPOTIFY_TOKEN_EXP_TIMESTAMP",
+  token_type: "SPOTIFY_TOKEN_TYPE",
+};
 export {};
