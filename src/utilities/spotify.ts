cae0bebd35d88e1512444cb650020b720c1134eb
Cody Constant <astraeusdev@gmail.com>
2023-02-20 03:15:38 +0000

    change(spotify-util): add fn to generate request validation string

--- src/utilities/spotify.ts
+++ src/utilities/spotify.ts
@@ -10,4 +10,15 @@ const STORAGE_KEYS: IStorageKeys = {
   exp_timestamp: "SPOTIFY_TOKEN_EXP_TIMESTAMP",
   token_type: "SPOTIFY_TOKEN_TYPE",
 };
+
+const validationString = (length: number = 12): string => {
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let text = "";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+};
 export {};
