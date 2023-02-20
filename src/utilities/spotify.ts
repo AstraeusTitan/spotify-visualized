5718203c2d401672a036a1f4f179f67add44be8c
Cody Constant <astraeusdev@gmail.com>
2023-02-20 03:57:50 +0000

    change(spotify-util): adjust property names

--- src/utilities/spotify.ts
+++ src/utilities/spotify.ts
@@ -2,13 +2,13 @@ const BASE_API_URL = "https://api.spotify.com/v1";
 
 interface IStorageKeys {
   accessToken: string;
-  exp_timestamp: string;
-  token_type: string;
+  expTimestamp: string;
+  tokenType: string;
 }
 const STORAGE_KEYS: IStorageKeys = {
   accessToken: "SPOTIFY_ACCESS_TOKEN",
-  exp_timestamp: "SPOTIFY_TOKEN_EXP_TIMESTAMP",
-  token_type: "SPOTIFY_TOKEN_TYPE",
+  expTimestamp: "SPOTIFY_TOKEN_EXP_TIMESTAMP",
+  tokenType: "SPOTIFY_TOKEN_TYPE",
 };
 
 const validationString = (length: number = 12): string => {
