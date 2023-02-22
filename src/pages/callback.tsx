d12d11a7a5aee8b3e56f03d31fda86b0a0b8783a
Cody Constant <astraeusdev@gmail.com>
2023-02-22 07:56:36 +0000

    feat(spotify-auth):
    -add callback route
    -add logic for handling auth callback and redirecting
    -add a stubbed page for redirecting

---
+++ src/pages/callback.tsx
@@ -0,0 +1,44 @@
+import { useSpotifyAuth } from "@/hooks/useSpotify";
+import { getState, TResponseData } from "@/utilities/spotify";
+import { useLayoutEffect } from "react";
+
+export async function getStaticProps() {
+  return {
+    props: {
+      successURL: "/details",
+      errorURL: "/",
+    },
+  };
+}
+
+const Callback = ({
+  successURL,
+  errorURL,
+}: {
+  successURL: string;
+  errorURL: string;
+}) => {
+  const spotify = useSpotifyAuth();
+  useLayoutEffect(() => {
+    const parent = window.opener;
+    spotify.handleCallback({
+      fragment: window.location.hash.substring(1),
+      query: window.location.search.substring(1),
+      state: getState(),
+      successFn: (response: TResponseData) => {
+        console.log(response);
+        parent.next.router.push(successURL);
+        window.close();
+      },
+      errorFn: (response: TResponseData) => {
+        console.error(response);
+        parent.next.router.push(errorURL);
+        window.close();
+      },
+    });
+  }, [spotify, successURL, errorURL]);
+
+  // TODO: Add a loading message
+  return <div>Callback page</div>;
+};
+export default Callback;
---
+++ src/pages/details.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Details: FC = () => {
+  return (
+    <div>
+      <h1>Show data</h1>
+    </div>
+  );
+};
+
+export default Details;
