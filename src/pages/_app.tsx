50fe704a81a7da7e5cf0012bda5aa72f37c1dc1c
Cody Constant <astraeusdev@gmail.com>
2023-02-22 01:20:03 +0000

    change(app): wrap app with spotify context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Spotify } from "@/components/Spotify";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Spotify.ContextProvider>
+      <Component {...pageProps} />
+    </Spotify.ContextProvider>
+  );
 }
