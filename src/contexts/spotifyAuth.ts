474eb0cc02fe95ea253cb89a9d185f7ff2f3103a
Cody Constant <astraeusdev@gmail.com>
2023-02-22 01:17:45 +0000

    feat(spotify-auth): create a context for exposing functions

--- src/contexts/spotifyAuth.ts
+++ src/contexts/spotifyAuth.ts
@@ -1,3 +1,12 @@
-import { createContext } from "react";
+import { Context, createContext } from "react";
 
-export const SpotifyAuthContext = createContext({});
+// TODO: set better types
+export interface ISpotifyContext {
+  openLoginPopup?: any;
+  logout?: any;
+  setSpotifyConfig?: any;
+  handleCallback?: any;
+}
+
+export const SpotifyAuthContext: Context<ISpotifyContext> =
+  createContext<ISpotifyContext>({});
