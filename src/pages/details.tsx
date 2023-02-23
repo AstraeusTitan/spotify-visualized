70fa7d58fa14049e05319c7c52bba5b975ba117c
Cody Constant <astraeusdev@gmail.com>
2023-02-23 03:16:27 +0000

    change(spotify-auth): add a check to redirect away from data page if token is invalid

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -1,6 +1,23 @@
-import { FC } from "react";
+import { useSpotify } from "@/hooks/useSpotify";
+import { tokenValid } from "@/utilities/spotify";
+import { useRouter } from "next/router";
+import { FC, useEffect } from "react";
 
 const Details: FC = () => {
+  const spotify = useSpotify();
+  const browser = typeof window !== "undefined";
+  const router = useRouter();
+
+  useEffect(() => {
+    if (spotify.setTokenValid) {
+      const check = tokenValid();
+      spotify.setTokenValid(check);
+      if (!check) {
+        router.push("/");
+      }
+    }
+  }, [browser, spotify, router]);
+
   return (
     <div>
       <h1>Show data</h1>
