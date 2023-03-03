8179aa99d38b78acf09bbf42d07fffdbc5757db6
Cody Constant <astraeusdev@gmail.com>
2023-03-03 22:50:51 +0000

    change: update title, description, and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,32 @@
 import { SpotifyProvider } from "@/contexts/spotify";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import * as ReactDOMServer from "react-dom/server";
+import Logo from "@/components/Logo";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SpotifyProvider>
-      <Component {...pageProps} />
-    </SpotifyProvider>
+    <>
+      <Head>
+        <title>Spotify Visualized</title>
+        <meta
+          name="description"
+          content="Visualize your Spotify listening data"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          href={`data:image/svg+xml,${encodeURIComponent(
+            ReactDOMServer.renderToStaticMarkup(
+              <Logo strokeStyle={{ stroke: "#38bdf8" }} />
+            )
+          )}`}
+        />
+      </Head>
+      <SpotifyProvider>
+        <Component {...pageProps} />
+      </SpotifyProvider>
+    </>
   );
 }
