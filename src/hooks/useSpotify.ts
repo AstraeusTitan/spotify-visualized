594dc5e94a781ec709325c34079f732293bdf212
Cody Constant <astraeusdev@gmail.com>
2023-03-10 23:09:37 +0000

    change(spotify): fix bad config passing

--- src/hooks/useSpotify.ts
+++ src/hooks/useSpotify.ts
@@ -2,27 +2,18 @@ import { SpotifyContext } from "@/contexts/spotify";
 import Spotify, { SpotifyConfig } from "@/utilities/Spotify";
 import { useContext, useEffect } from "react";
 
-type Props = {
-  clientId?: string;
-  redirectUri?: string;
-};
-export const useSpotify = ({ clientId, redirectUri }: Props = {}) => {
+export const useSpotify = (config: SpotifyConfig = {}) => {
   const { spotify, setSpotify } = useContext(SpotifyContext);
   useEffect(() => {
-    console.info("Inside useSpotify");
     if (spotify === undefined && setSpotify) {
-      console.info("spotify undefined");
-      const config: SpotifyConfig = {
-        clientId,
-        redirectUri,
+      const s = new Spotify({
+        ...config,
         storage: window.localStorage,
         fetch: fetch.bind(window),
-      };
-      const s = new Spotify(config);
+      });
       s?.Auth.loadToken(window.localStorage);
       setSpotify(s);
     }
-    console.info("spotify", spotify);
-  }, [clientId, redirectUri, setSpotify, spotify]);
+  }, [config, setSpotify, spotify]);
   return { spotify, setSpotify };
 };
