25f9c7f7d0ca86fb3be3171c5fbff3a4aaa6809f
Cody Constant <astraeusdev@gmail.com>
2023-03-11 05:08:53 +0000

    feat: add a basic profile page

---
+++ src/pages/me/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "@/components/Container";
+import TopArtists from "@/components/TopArtists";
+import { useSpotify } from "@/hooks/useSpotify";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const Me = () => {
+  const router = useRouter();
+  const { spotify } = useSpotify();
+
+  useEffect(() => {
+    if (spotify && !spotify.Auth.tokenValid()) {
+      router.push("/");
+    }
+  }, [router, spotify]);
+
+  return (
+    <main>
+      <Container>
+        <div className="py-8">
+          <TopArtists
+            time_range="short_term"
+            title="Top Artists"
+            indexRoute="/me/top/artists"
+            itemRoute="/artist"
+          />
+        </div>
+      </Container>
+    </main>
+  );
+};
+
+export default Me;
