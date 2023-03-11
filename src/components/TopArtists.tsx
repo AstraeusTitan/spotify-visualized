f7870ff0f973da87ca91796a6aac27409dd06a31
Cody Constant <astraeusdev@gmail.com>
2023-03-11 03:17:41 +0000

    change(artist): add catch on fetch

--- src/components/TopArtists.tsx
+++ src/components/TopArtists.tsx
@@ -4,9 +4,11 @@ import { useEffect, useState } from "react";
 import Artist from "./Artist";
 
 const TopArtists = ({
+  title = true,
   time_range = "short_term",
 }: {
   time_range?: "short_term" | "medium_term" | "long_term";
+  title?: boolean;
 }) => {
   const { spotify } = useSpotify();
   const [data, setData] = useState<Api.Artist[] | undefined>(undefined);
@@ -17,13 +19,15 @@ const TopArtists = ({
         limit: 4,
         time_range: time_range,
       });
-      result.then((json) => setData(json.items as Api.Artist[]));
+      result
+        .then((json) => setData(json.items as Api.Artist[]))
+        .catch((reason) => console.info(reason));
     }
   }, [spotify, time_range]);
 
   return (
     <Artist.Grid
-      title="Top Artists"
+      title={title ? "Top Artists" : undefined}
       artists={data}
       indexRoute="/me/top/artists"
       itemRoute="/artist"
