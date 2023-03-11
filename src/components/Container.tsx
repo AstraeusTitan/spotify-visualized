15ac70d29d0689f0c800ecb41bc968480b29d133
Cody Constant <astraeusdev@gmail.com>
2023-03-11 05:04:37 +0000

    change: change container markup

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,29 +1,7 @@
-import { FC, ReactNode, PropsWithChildren, HTMLAttributes } from "react";
-import { clsx } from "clsx";
-
-const Outer: FC<PropsWithChildren<HTMLAttributes<HTMLDivElement>>> = ({
-  children,
-  className,
-}) => {
-  const baseClasses = "mx-auto max-w-7xl sm:px-8 lg:px-8";
-  return <div className={clsx(baseClasses, className)}>{children}</div>;
-};
-
-const Inner: FC<PropsWithChildren<HTMLAttributes<HTMLDivElement>>> = ({
-  children,
-  className,
-}) => {
-  const baseClasses = "mx-auto max-w-2xl lg:max-w-5xl px-4 sm:px-8 lg:px-12";
-  return <div className={clsx(baseClasses, className)}>{children}</div>;
-};
+import { PropsWithChildren } from "react";
 
 export const Container = ({ children }: PropsWithChildren) => {
   return (
-    <Outer>
-      <Inner>{children}</Inner>
-    </Outer>
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8">{children}</div>
   );
 };
-
-Container.Outer = Outer;
-Container.Inner = Inner;
