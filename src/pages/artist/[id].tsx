23b3c315c3b3541f19604de856f39f55d6c9b295
Cody Constant <astraeusdev@gmail.com>
2023-04-09 04:00:38 +0000

    change(artist): add album lists to artist details page

--- src/pages/artist/[id].tsx
+++ src/pages/artist/[id].tsx
@@ -1,7 +1,9 @@
+import AlbumList from "@/components/Album/AlbumList";
 import { Container } from "@/components/Container";
 import DescriptionList from "@/components/Shared/DescriptionList";
 import ArtistsTopTracks from "@/components/Track/ArtistsTopTracks";
 import { useSpotify } from "@/hooks/useSpotify";
+import { Album } from "@/utilities/Spotify/Api/albums";
 import { Artist } from "@/utilities/Spotify/Api/artists";
 import Head from "next/head";
 import Image from "next/image";
@@ -14,6 +16,12 @@ const ArtistDetails = () => {
   const { id } = router.query;
   const { spotify } = useSpotify();
   const [artist, setArtist] = useState<Artist | undefined>(undefined);
+  const [singles, setSingles] = useState<Album[] | undefined>(undefined);
+  const [albums, setAlbums] = useState<Album[] | undefined>(undefined);
+  const [appearsOn, setAppearsOn] = useState<Album[] | undefined>(undefined);
+  const [compilations, setCompilations] = useState<Album[] | undefined>(
+    undefined
+  );
 
   useEffect(() => {
     if (spotify) {
@@ -28,6 +36,41 @@ const ArtistDetails = () => {
         .catch((reason) => console.info(reason));
     }
   }, [id, spotify]);
+
+  useEffect(() => {
+    const storeResult = (result: Promise<any>, storeFn: Function) => {
+      result
+        .then((json) => {
+          // TODO: do something once an error is identified
+          if (!json.error) {
+            storeFn(json.items);
+          }
+        })
+        .catch((reason) => console.info(reason));
+    };
+    if (spotify && artist) {
+      const singlesResult = spotify.Api.getArtistsAlbums(artist.id, {
+        include_groups: ["single"],
+        limit: 50,
+      });
+      const albumsResult = spotify.Api.getArtistsAlbums(artist.id, {
+        include_groups: ["album"],
+        limit: 50,
+      });
+      const appearsOnResult = spotify.Api.getArtistsAlbums(artist.id, {
+        include_groups: ["appears_on"],
+        limit: 50,
+      });
+      const compilationsResult = spotify.Api.getArtistsAlbums(artist.id, {
+        include_groups: ["compilation"],
+        limit: 50,
+      });
+      storeResult(singlesResult, setSingles);
+      storeResult(albumsResult, setAlbums);
+      storeResult(appearsOnResult, setAppearsOn);
+      storeResult(compilationsResult, setCompilations);
+    }
+  }, [spotify, artist]);
 
   return (
     <main className="mt-8">
@@ -120,10 +163,36 @@ const ArtistDetails = () => {
             </DescriptionList.Item>
 
             <DescriptionList.Item>
+              <DescriptionList.Item.Name>Singles</DescriptionList.Item.Name>
+              <DescriptionList.Item.Description>
+                {/* TODO: Figure out how to make a track list look nice */}
+                <AlbumList albums={singles} />
+              </DescriptionList.Item.Description>
+            </DescriptionList.Item>
+
+            <DescriptionList.Item>
               <DescriptionList.Item.Name>Albums</DescriptionList.Item.Name>
               <DescriptionList.Item.Description>
                 {/* TODO: Figure out how to make a track list look nice */}
-                Album list goes here
+                <AlbumList albums={albums} />
+              </DescriptionList.Item.Description>
+            </DescriptionList.Item>
+
+            <DescriptionList.Item>
+              <DescriptionList.Item.Name>Appears On</DescriptionList.Item.Name>
+              <DescriptionList.Item.Description>
+                {/* TODO: Figure out how to make a track list look nice */}
+                <AlbumList albums={appearsOn} />
+              </DescriptionList.Item.Description>
+            </DescriptionList.Item>
+
+            <DescriptionList.Item>
+              <DescriptionList.Item.Name>
+                Compilations
+              </DescriptionList.Item.Name>
+              <DescriptionList.Item.Description>
+                {/* TODO: Figure out how to make a track list look nice */}
+                <AlbumList albums={compilations} />
               </DescriptionList.Item.Description>
             </DescriptionList.Item>
           </DescriptionList.List>
